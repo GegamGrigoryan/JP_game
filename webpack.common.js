6451897de722c4f7dbf82e1324233affa89dab01
Test <m-mik@ro.ru>
2023-06-14 00:15:09 +0300

    ==

--- webpack.common.js
+++ webpack.common.js
@@ -33,6 +33,7 @@ module.exports = {
         use: [
           MiniCssExtractPlugin.loader, 'css-loader',
         ],
+        exclude: /node_modules/,
         sideEffects: true,
       },
       {
